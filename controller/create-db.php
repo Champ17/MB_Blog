a337e827491223881e81a7f7071692d376caaa67
Miguel Banuelos <mistergengar@gmail.com>
2015-01-28 09:20:05 -0800

    Done with Milestone 2

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,8 +1,13 @@
 <?php
+   require_once ("../model/database.php");
+   
+   $connection = new mysqli($host, $username, $password); 
+   
+   if($connection->connect_error){
+      die("Error: " . $connection->connect_error);
+   }
+   else{
+       echo "Success" .$connection->host_info;
+   }
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$connection->close();
