fc21dee11b632ecbe9d65960f8d63b118f6f8ec4
Miguel Banuelos <mistergengar@gmail.com>
2015-02-02 10:45:58 -0800

    asdd

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-
-        <?php
-        require_once (__DIR__ ."/controller/create-db.php");
-        ?>
+<?php
+    require_once(__DIR__ . "/view/header.php");
+    require_once(__DIR__ . "/controller/create-db.php");
+    require_once(__DIR__ . "/view/footer.php");
+?>
     
- 
